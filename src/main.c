3ebb676eb828d876eafc61a47e5c01b5324ceedd
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-04-12 22:21:46 +0200

    Initialize SDL and window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
-int main() {
-    printf("Hello world!");
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+bool isRunning = false;
+
+void initializeSDL()
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    window = SDL_CreateWindow(
+        "My engine",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        0);
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    isRunning = true;
+}
+
+void processInput()
+{
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        isRunning = false;
+        break;
+
+    case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_ESCAPE)
+        {
+            isRunning = false;
+        }
+        break;
+    }
+}
+
+int main()
+{
+    initializeSDL();
+
+    while (isRunning)
+    {
+        processInput();
+    }
+
     return 0;
 }
