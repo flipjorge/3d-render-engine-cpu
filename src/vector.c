ac4cbd789dc4578de53ec1a12fd38684489a8bf0
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-05-03 11:14:55 +0200

    Implement vector sum and multiple methods

--- src/vector.c
+++ src/vector.c
@@ -1,2 +1,21 @@
 #include "vector.h"
 
+vector2_t vector2Sum(vector2_t a, vector2_t b)
+{
+    return (vector2_t){ a.x + b.x, a.y + b.y };
+}
+
+vector2_t vector2Multiple(vector2_t a, vector2_t b)
+{
+    return (vector2_t){ a.x * b.x, a.y * b.y };
+}
+
+vector3_t vector3Sum(vector3_t a, vector3_t b)
+{
+    return (vector3_t){ a.x + b.x, a.y + b.y, a.z + b.z };
+}
+
+vector3_t vector3Multiple(vector3_t a, vector3_t b)
+{
+    return (vector3_t){ a.x * b.x, a.y * b.y, a.z * b.z };
+}
--- src/vector.h
+++ src/vector.h
@@ -12,4 +12,10 @@ typedef struct {
     float z;
 } vector3_t;
 
+vector2_t vector2Sum(vector2_t a, vector2_t b);
+vector2_t vector2Multiple(vector2_t a, vector2_t b);
+
+vector3_t vector3Sum(vector3_t a, vector3_t b);
+vector3_t vector3Multiple(vector3_t a, vector3_t b);
+
 #endif
