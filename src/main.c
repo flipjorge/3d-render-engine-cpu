dfb6c4513de1660b9fc377b34cca3ff02cc93431
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-05-06 22:00:09 +0200

    Fix fps and implement delta/frameTime

--- src/main.c
+++ src/main.c
@@ -6,10 +6,15 @@
 #include "projection.h"
 #include "cube.h"
 
+#define TARGET_FRAME_RATE 60
+#define TARGET_FRAME_TIME (1000 / TARGET_FRAME_RATE)
+
 bool isRunning = false;
 
 cube_t cube;
 vector2_t cubeProjectedPoints[8];
+
+Uint32 previousFrameTicks;
 
 void setup()
 {
@@ -23,6 +28,8 @@ void setup()
         windowHeight);
 
     createCube(&cube, 5, (vector3_t){ 0, 0, 0 });
+
+    previousFrameTicks = SDL_GetTicks();
 }
 
 void processInput()
@@ -47,8 +54,20 @@ void processInput()
 
 void update()
 {
+    int frameTime = SDL_GetTicks() - previousFrameTicks;
+    
+    if(frameTime < TARGET_FRAME_TIME)
+    {
+        SDL_Delay(TARGET_FRAME_TIME - frameTime);
+        frameTime = SDL_GetTicks() - previousFrameTicks;
+    }
+
+    float frameTimeSeconds = frameTime / 1000.0f;
+    previousFrameTicks = SDL_GetTicks();
+    
+    float rotationIncrement = 1 * frameTimeSeconds;
     cube.position = (vector3_t){ 0, 0, 30 };
-    cube.rotation = vector3Sum( cube.rotation, (vector3_t){ .001, .001, .001 } );
+    cube.rotation = vector3Sum( cube.rotation, (vector3_t){ rotationIncrement, rotationIncrement, rotationIncrement } );
     vector3_t cubeTranformedVertices[8];
     getCubeTransformedVertices(&cube, cubeTranformedVertices);
 
