39ddb2f80e4f82e8259e074c8cee1138ef3bdbc4
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-05-06 23:21:58 +0200

    Implement drawTriangle

--- src/display.h
+++ src/display.h
@@ -21,5 +21,6 @@ void drawPixel(int x, int y, uint32_t color);
 void drawGrid(uint8_t cellSize, uint32_t color);
 void drawRectangle(int x, int y, int width, int height, uint32_t color);
 void drawLine(int x0, int y0, int x1, int y1, uint32_t color);
+void drawTriangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 
 #endif
