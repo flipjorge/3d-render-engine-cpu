4ba4e08f20ebb299cdbd9d52bb4c0e960b842eb3
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-05-20 23:36:47 +0200

    Use matrices to get transformed vertices

--- src/mesh.c
+++ src/mesh.c
@@ -1,20 +1,19 @@
-#include "mesh.h"
 #include <stddef.h>
+#include "mesh.h"
+#include "matrix.h"
 
 void getMeshTransformedVertices(const mesh_t* mesh, vector3_t** tranformedVertices)
 {
     const int numberVertices = array_length(mesh->vertices);
 
+    matrix4_t scale = matrix4Scale(&mesh->scale);
+    matrix4_t rotation = matrix4Rotation(&mesh->rotation);
+    matrix4_t translation = matrix4Translation(&mesh->position);
+    matrix4_t transform = matrix4TRS(&scale, &rotation, &translation);
+
     for (size_t i = 0; i < numberVertices; i++)
     {
-        vector3_t vertice;
-
-        vertice = vector3Multiple(mesh->vertices[i], mesh->scale);
-        vertice = vector3RotateX(vertice, mesh->rotation.x);
-        vertice = vector3RotateY(vertice, mesh->rotation.y);
-        vertice = vector3RotateZ(vertice, mesh->rotation.z);
-        vertice = vector3Sum(vertice, mesh->position);
-
+        vector3_t vertice = matrix4MultiplyVector(&transform, &mesh->vertices[i]);
         array_push(*tranformedVertices, vertice);
     }
 }
