92a5cd7cb0f3acd17c9b7be402404e8e2437d6c2
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-05-26 19:21:16 +0200

    Add texture properties to triangle, faces and cube mesh

--- src/triangle.h
+++ src/triangle.h
@@ -3,13 +3,16 @@
 
 #include <SDL2/SDL.h>
 #include "vector.h"
+#include "texture.h"
 
 typedef struct {
     int a, b, c;
+    texture_t aUV, bUV, cUV;
 } face_t;
 
 typedef struct {
     vector2_t points[3];
+    texture_t textureCoordinates[3];
     float depth;
     uint32_t color;
 } triangle_t;
