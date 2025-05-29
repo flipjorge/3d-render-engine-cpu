4f83476b23e882aac32ee4c36b2ef946c6ed1be4
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-05-29 20:23:02 +0200

    Create ARGB to RGBA method

--- src/texture.h
+++ src/texture.h
@@ -3,10 +3,15 @@
 
 #include <stdint.h>
 
+#define TEXTURE_WIDTH 64
+#define TEXTURE_HEIGHT 64
+
 typedef struct {
     float u;
     float v;
 } texture_t;
+
+uint32_t* convertARGBtoRGBATexture(const uint32_t* src, int numPixels);
 
 extern const uint8_t sampleTexture[];
 
