b7050980cde1d907f4954f02d09c5638a3197d88
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-05-31 19:13:55 +0200

    Parse vt values

--- src/obj.c
+++ src/obj.c
@@ -11,6 +11,8 @@ void loadMeshFromObj(mesh_t* mesh, char* filename)
     mesh->vertices = NULL;
     mesh->faces = NULL;
     
+    texture_t* textures = NULL;
+
     while(fgets(line, 256, file))
     {
         if(strncmp(line, "v ", 2) == 0)
@@ -20,10 +22,33 @@ void loadMeshFromObj(mesh_t* mesh, char* filename)
             array_push(mesh->vertices, vertice);
         }
 
+        if(strncmp(line, "vt ", 2) == 0)
+        {
+            texture_t texture;
+            sscanf(line, "vt %f %f", &texture.u, &texture.v);
+            array_push(textures, texture);
+        }
+
         if(strncmp(line, "f ", 2) == 0)
         {
-            face_t face;
-            sscanf(line, "f %i %i %i", &face.a, &face.b, &face.c);
+            int verticeIndices[3];
+            int textureIndices[3];
+            
+            sscanf(line, "f %i/%i %i/%i %i/%i",
+                &verticeIndices[0], &textureIndices[0],
+                &verticeIndices[1], &textureIndices[1],
+                &verticeIndices[2], &textureIndices[2]
+            );
+            
+            face_t face = {
+                .a = verticeIndices[0],
+                .b = verticeIndices[1],
+                .c = verticeIndices[2],
+                .aUV = textures[textureIndices[0] - 1],
+                .bUV = textures[textureIndices[1] - 1],
+                .cUV = textures[textureIndices[2] - 1]
+            };
+
             array_push(mesh->faces, face);
         }
     }
