b43d359f0b317ca897b93ce9f62e6895906d25eb
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2025-10-06 17:03:48 +0200

    Add documentation to camera

--- src/camera.h
+++ src/camera.h
@@ -3,6 +3,9 @@
 
 #include "vector.h"
 
+// Represents the camera in the 3D scene, defining the viewpoint for rendering.
+// The view matrix, which transforms world coordinates into camera space, is derived
+// from the camera's properties.
 typedef struct {
     vector3_t position;
     vector3_t direction;
